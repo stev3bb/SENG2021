64036d1cc582a43c4aa2a6c167c7974d4eed449b
Nelson <nelson.susanto@student.unsw.edu.au>
2016-09-17 19:50:08 +1000

    ?

--- js/home-search/home-search.component.js
+++ js/home-search/home-search.component.js
@@ -2,7 +2,7 @@ angular.
     module('homeSearch').
     component('homeSearch', {
         templateUrl: 'js/home-search/home-search.template.html',
-        controller: function NavController() {
+        controller: function HomeSearchController() {
             console.log("This works");
         }
     });
