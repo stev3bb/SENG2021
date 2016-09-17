e6e38edfe98684d28565200a8dd328d4d3c7e69a
Nelson <nelson.susanto@student.unsw.edu.au>
2016-09-17 12:01:30 +1000

    yee home search redirects

--- js/MainController.js
+++ js/MainController.js
@@ -2,6 +2,13 @@
     var app = angular.module("main", []);
 
     var MainController = function($scope) {
+        $scope.campsiteSearch = function(location) {
+            console.log(location);
+            if (location != undefined) {
+                document.location.href = "campsearch.html";
+            }
+        }
+
         $scope.message = "Campsite Search";
     };
 
