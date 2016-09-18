a686745bbb31f83b1b553fb569e40dbe68fd2f2b
Nelson <nelson.susanto@student.unsw.edu.au>
2016-09-18 17:34:47 +1000

    ready for node

--- SENG_nodejs/app.js
+++ SENG_nodejs/app.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 
 var home = require('./routes/home');
 var campsearch = require('./routes/campsearch');
+var popularcampsites = require('./routes/popularcampsites');
+var equipment = require('./routes/equipment');
+var guides = require('./routes/guides');
 var campsites = require('./routes/campsites');
 
 var app = express();
@@ -27,6 +30,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', home);
 app.use('/campsearch', campsearch);
+app.use('/popularcampsites', popularcampsites);
+app.use('/equipment', equipment);
+app.use('/guides', guides);
 app.use('/campsites', campsites);
 
 // catch 404 and forward to error handler
--- SENG_nodejs/routes/equipment.js
+++ SENG_nodejs/routes/equipment.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/:title', function(req, res, next) {
-    res.render('campsites', {
-        title: req.params.title,
+router.get('/', function(req, res, next) {
+    res.render('equipment', {
+        title: 'Equipment',
         partials: {header: 'partials/header', navbar: 'partials/navbar', bottomJs: 'partials/bottomJs'}
     });
 });
