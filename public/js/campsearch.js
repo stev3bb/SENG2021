90d8ae6fd6a94145acb6f155ff2136efdc557197
steve <stev3bb@gmail.com>
2016-09-18 22:33:46 +1000

    added some stolen functions from google doc

--- public/js/campsearch.js
+++ public/js/campsearch.js
@@ -132,6 +132,40 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         'Your browser doesn\'t support geolocation.');
 }
 
+//stolen functions mahahahaha
+
+// Adds a marker to the map and push to the array.
+function addMarker(location) {
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+    markers.push(marker);
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+    setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+    setMapOnAll(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+}
+
 /*
 search function
 summary:
