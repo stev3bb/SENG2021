2ea9c334e0487b934fc38ce68b1ef1492fcb1486
Nelson <nelson.susanto@student.unsw.edu.au>
2016-09-20 00:17:52 +1000

    mongoose

--- routes/campsearch.js
+++ routes/campsearch.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://seng:seng@ds035846.mlab.com:35846/seng2021');
+
+var campsiteSchema = new mongoose.Schema({
+    id: String,
+    name: String,
+    address: String
+});
+
+var Campsite = mongoose.model('Campsite', campsiteSchema);
+var testSite = Campsite({
+    id: 'ChIJPel2yVLHEmsR4Iw4Si2c5Gc',
+    name: 'Woronora Village Tourist Park',
+    address: '1 Menai Rd, Woronora NSW 2232, Australia'
+}).save(function(err) {
+    if (err) throw err;
+    console.log('Item saved to DB');
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
