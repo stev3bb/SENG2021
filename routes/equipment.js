e529a8e0469929219cf703a0ba822e6c475ae2b0
Nelson <nelson.susanto@student.unsw.edu.au>
2016-09-20 01:02:25 +1000

    equipment

--- routes/equipment.js
+++ routes/equipment.js
@@ -9,4 +9,11 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.get('/:equip', function(req, res, next) {
+    res.render('equipment_individual', {
+        title: req.params.equip,
+        partials: {header: 'partials/header', navbar: 'partials/navbar', bottomJs: 'partials/bottomJs'}
+    });
+});
+
 module.exports = router;
