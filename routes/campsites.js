de3ede818eab43ec206b96ba200e958ced99bb64
steve <stev3bb@gmail.com>
2016-10-07 18:58:48 +1100

    install request module

--- routes/campsites.js
+++ routes/campsites.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express();
+var request = require('request');
+
+//url links 
 
 /*
 var mongoose = require('mongoose');
@@ -14,6 +17,11 @@ var campsiteSchema = new mongoose.Schema({
 
 var Campsite = mongoose.model('Campsite', campsiteSchema);
 */
+        /*request('google.com.au', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body) // Show the HTML for the Google homepage. 
+            }
+        })*/
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -28,14 +36,16 @@ router.get('/', function(req, res, next) {
     var weatherapi ='http://api.openweathermap.org/data/2.5/weather?zip=+'+match[1]+',au&appid=4d30a475c46e1fc7e5c6d9f7ee6517be&mode=json';
 
 
-    res.render('campsites', {
+        /*
+        //used for google maps
         place_id: req.param('id'),
-        place_name: 'something',
-        place_min: 1,
-        place_max: 2,
-        place_wind_speed: 3,
-        partials: {header: 'partials/header', navbar: 'partials/navbar', bottomJs: 'partials/bottomJs', API_KEY: 'partials/api_key'}
-    });
+        place_name: 1,
+        place_condition: 2,
+        place_min: 3,
+        place_max: 4,
+        place_wind_speed: 5,
+        */
+ 
 });
 
 module.exports = router;
