1b97cdc36444d88d79925040735028822f5d9cdc
steve <stev3bb@gmail.com>
2016-10-07 19:35:36 +1100

    weather is done

--- routes/campsites.js
+++ routes/campsites.js
@@ -1,6 +1,8 @@
+//load some node js modules
 var express = require('express');
 var router = express();
 var request = require('request');
+//var request = require('request');
 
 //url links 
 
@@ -17,35 +19,46 @@ var campsiteSchema = new mongoose.Schema({
 
 var Campsite = mongoose.model('Campsite', campsiteSchema);
 */
-        /*request('google.com.au', function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(body) // Show the HTML for the Google homepage. 
-            }
-        })*/
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    /*
-    Campsite.findOne({id: req.params.id}, function(err, data) {
-        if (err) throw err;
-    })*/
+/*
+Campsite.findOne({id: req.params.id}, function(err, data) {
+    if (err) throw err;
+})*/
     var regrex = /.*\, .* .* (\d+)\, \w+/g;
     var suburb = req.param('address');
     var match = regrex.exec(suburb);
 
-    var weatherapi ='http://api.openweathermap.org/data/2.5/weather?zip=+'+match[1]+',au&appid=4d30a475c46e1fc7e5c6d9f7ee6517be&mode=json';
+    var weatherapi ='http://api.openweathermap.org/data/2.5/weather?zip=+'+match[1]+',au&appid=4d30a475c46e1fc7e5c6d9f7ee6517be&mode=json&units=metric';
 
+    request(
+        {   url: weatherapi,
+            json: true
+        }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body) 
+            res.render('campsites', {
+                //used for google maps
+                place_id: req.param('id'),
+                place_name: body.name,
+                place_condition: body.weather[0].description,
+                place_min: body.main.temp_min,
+                place_max: body.main.temp_max,
+                place_wind_speed: body.wind.speed,
+                partials: {header: 'partials/header', navbar: 'partials/navbar', bottomJs: 'partials/bottomJs', API_KEY: 'partials/api_key'}
+            });     
+        }else{
+            //render something else 
+        }
+    })
 
-        /*
-        //used for google maps
-        place_id: req.param('id'),
-        place_name: 1,
-        place_condition: 2,
-        place_min: 3,
-        place_max: 4,
-        place_wind_speed: 5,
-        */
- 
+    
 });
+
+
+        
+
+
 
 module.exports = router;
