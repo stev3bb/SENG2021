0abdd3093fceef7454c75c1c820967a900085e36
Nelson <nelson.susanto@student.unsw.edu.au>
2016-10-15 14:29:12 +1100

    google maps node.js client

---
+++ node_modules/@google/maps/lib/internal/throttled-queue.js
@@ -0,0 +1,91 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var CircularBuffer = require('./circular-buffer');
+var Task = require('./task');
+
+exports.inject = function(setTimeout, getTime) {
+  return {
+    /**
+     * Creates a ThrottledQueue. The queue stores tasks, which will be executed
+     * asynchronously, at a controlled rate.
+     *
+     * @param {number} limit The maximum number of tasks that can be executed
+     *     over one period.
+     * @param {number} period The time period (ms) over which limit is
+     *     enforceable.
+     * @return {ThrottledQueue}
+     */
+    create: function(limit, period) {
+      var me = {};
+      var queue = [];
+      var recentTimes = CircularBuffer.create(limit);
+      var scheduled = false;
+
+      function schedule() {
+        if (scheduled) return;
+
+        var lastTime = recentTimes.item(limit - 1);
+        var delay = lastTime + period - getTime();
+        delay = (lastTime != undefined && delay > 0) ? delay : 0;
+
+        scheduled = true;
+        setTimeout(function() {
+          scheduled = false;
+
+          var action = queue.shift();
+          if (action) action();
+
+          if (queue.length) {
+            schedule();
+          }
+        }, delay);
+      }
+
+      /**
+       * Adds a task to the work queue.
+       *
+       * @param {function(): Task<T>} doSomething Starts the task. This function
+       *     will be called when the rate limit allows.
+       * @return {Task<T>} The delayed task.
+       * @template T
+       */
+      me.add = function(doSomething) {
+        schedule();
+
+        return Task.start(function(resolve) {
+          var cancelled;
+
+          // Call the callback when the action is popped off the queue
+          // (unless we were cancelled in the meantime).
+          queue.push(function() {
+            if (!cancelled) {
+              recentTimes.insert(getTime());
+              resolve();
+            }
+          });
+          return function onCancel() {
+            cancelled = true;
+          };
+        })
+        .thenDo(doSomething);
+      };
+
+      return me;
+    }
+  };
+};
---
+++ node_modules/@google/maps/spec/unit/throttled-queue-spec.js
@@ -0,0 +1,210 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var Task = require('../../lib/internal/task');
+
+describe('throttle', function() {
+  var PERIOD = 1000;
+
+  // This simplistic fake is suitable for injecting into the ThrottledQueue.
+  // The queue only has one pending timeout at a time.
+  var fakeSetTimeout = function(callback, duration) {
+    setImmediate(function() {
+      theTime += duration;
+      callback();
+    });
+  };
+  var theTime, createQueue, queue, doSomething;
+  beforeEach(function() {
+    theTime = 1000000;
+    createQueue = require('../../lib/internal/throttled-queue')
+        .inject(fakeSetTimeout, function() {
+          return theTime;
+        })
+        .create;
+    queue = createQueue(1, PERIOD);
+
+    var count = 0;
+    doSomething = jasmine.createSpy('doSomething')
+        .and.callFake(function() {
+          ++count;
+          return Task.withValue('result ' + count);
+        });
+  });
+
+  it('doesn\'t do the operation synchronously', function() {
+    queue.add(doSomething);
+    expect(doSomething).not.toHaveBeenCalled();
+  });
+
+  it('calls doSomething', function(done) {
+    queue.add(doSomething)
+    .finally(function() {
+      expect(doSomething).toHaveBeenCalled();
+      done();
+    });
+  });
+
+  it('.cancel() cancels an operation', function(done) {
+    queue.add(doSomething)
+    .thenDo(fail, fail)
+    .finally(function() {
+      expect(doSomething).not.toHaveBeenCalled();
+      done();
+    })
+    .cancel();
+  });
+
+  it('does actions in order', function(done) {
+    queue.add(doSomething)
+    .thenDo(function(result) {
+      expect(result).toBe('result 1');
+    }, fail);
+
+    queue.add(doSomething)
+    .thenDo(function(result) {
+      expect(result).toBe('result 2');
+    }, fail);
+
+    queue.add(doSomething)
+    .thenDo(function(result) {
+      expect(result).toBe('result 3');
+    })
+    .thenDo(done, fail);
+  });
+
+  it('does it immediately the first time', function(done) {
+    var startTime = theTime;
+
+    queue.add(doSomething)
+    .thenDo(function() {
+      expect(theTime).toBe(startTime);
+      done();
+    });
+  });
+
+  it('spaces out calls made at the same time', function(done) {
+    var startTime = theTime;
+
+    queue.add(doSomething)
+    .thenDo(function() {
+      expect(theTime).toBe(startTime);
+    }, fail);
+
+    queue.add(doSomething)
+    .thenDo(function() {
+      expect(theTime).toBe(startTime + PERIOD);
+    }, fail);
+
+    queue.add(doSomething)
+    .thenDo(function() {
+      expect(theTime).toBe(startTime + 2 * PERIOD);
+    })
+    .thenDo(done, fail);
+  });
+
+  it('spaces out calls made half a PERIOD apart', function(done) {
+    var startTime = theTime;
+
+    queue.add(doSomething)
+    .thenDo(function() {
+      expect(theTime).toBe(startTime);
+    }, fail);
+
+    setTimeout(function() {
+      theTime += 0.5 * PERIOD;
+      queue.add(doSomething)
+      .thenDo(function() {
+        expect(theTime).toBe(startTime + PERIOD);
+      })
+      .thenDo(done, fail);
+    }, 10);
+  });
+
+  it('doesn\'t wait when calls are made far apart', function(done) {
+    var startTime = theTime;
+
+    queue.add(doSomething)
+    .thenDo(function() {
+      expect(theTime).toBe(startTime);
+    }, fail);
+
+    setTimeout(function() {
+      theTime += 2 * PERIOD;
+      queue.add(doSomething)
+      .thenDo(function() {
+        expect(theTime).toBe(startTime + 2 * PERIOD);
+      })
+      .thenDo(done, fail);
+    }, 10);
+  });
+
+  it('does not wait for calls that are cancelled', function(done) {
+    var startTime = theTime;
+
+    queue.add(doSomething)
+    .thenDo(function(result) {
+      expect(theTime).toBe(startTime);
+      expect(result).toBe('result 1');
+    }, fail);
+
+    queue.add(doSomething)
+    .thenDo(fail, fail)
+    .finally(function() {
+      expect(theTime).toBe(startTime);
+    })
+    .cancel();
+
+    queue.add(doSomething)
+    .thenDo(function(result) {
+      expect(theTime).toBe(startTime + PERIOD);
+      expect(result).toBe('result 2');
+    })
+    .thenDo(done, fail);
+  });
+
+  describe('when limit is 3', function() {
+    beforeEach(function() {
+      queue = createQueue(3, PERIOD);
+    });
+
+    it('waits before making the 4th call made together', function(done) {
+      var startTime = theTime;
+
+      queue.add(doSomething)
+      .thenDo(function() {
+        expect(theTime).toBe(startTime);
+      }, fail);
+
+      queue.add(doSomething)
+      .thenDo(function() {
+        expect(theTime).toBe(startTime);
+      }, fail);
+
+      queue.add(doSomething)
+      .thenDo(function() {
+        expect(theTime).toBe(startTime);
+      }, fail);
+
+      queue.add(doSomething)
+      .thenDo(function() {
+        expect(theTime).toBe(startTime + PERIOD);
+      })
+      .thenDo(done, fail);
+    });
+  });
+});
