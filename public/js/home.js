48aad3f95461a1fd896e885c7e743c3309e47b37
Nelson <nelson.susanto@student.unsw.edu.au>
2016-10-17 22:08:51 +1100

    added validation check to homepage

--- public/js/home.js
+++ public/js/home.js
@@ -3,9 +3,13 @@ var autocomplete;
 
 function querySearch() {
     var place = autocomplete.getPlace();
-    if (place.place_id) {
+    if (place && place.place_id) {
         var loc = place.place_id;
         document.location.href = '/campsearch?location=' + loc;
+    } else {
+        console.log('ya');
+        var input = document.getElementById('location').value;
+        document.getElementById('invalid').innerHTML = "'<span style='color:red'>" + input + "</span>' is not a valid Google place. Please select an option from the dropdown list.";
     }
 }
 
